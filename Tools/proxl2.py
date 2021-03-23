5d214e653fdf8d61e60a8954992ccfcfe5088b77
mamar98 <55411197+mamar98@users.noreply.github.com>
2021-03-23 19:07:24 +0100

    correct error in proxl2

--- Tools/proxl2.py
+++ Tools/proxl2.py
@@ -1,9 +1,3 @@
-function [z] = proxl2(x,y,eta)
-    %%% projection onto the l2 ball
-    t = x - y;
-    s = t*min(eta/norm(t,2),1);
-    z = x + s - t ;
-end
 import numpy as np
 
 def proxl2(x,y,eta):
