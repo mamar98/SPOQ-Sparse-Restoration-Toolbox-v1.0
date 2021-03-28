47d5aca3cc1d693c64f7d424e89527ebbb315067
Aurelien Pejout <Aurelien.Pejout@supelec.fr>
2021-03-28 15:15:49 +0200

    Correctif norm2

--- Tools/norm2.py
+++ Tools/norm2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def norm2(K,N):
     nbiter = 50
-    b = np.random.rand((N,1))
+    b = np.random.rand(N,1)
     for i in range(nbiter):
         tmp = K.T @ (K @ b)
         tmpnorm = np.linalg.norm(tmp)
